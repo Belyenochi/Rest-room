21f748504656c986999a0a9037b1ba8c2ef98f24
Asus <754572491@qq.com>
2017-05-02 21:40:15 +0800

    2017.5.2.21.40

--- 数据库设计/数据库修改1.sql
+++ 数据库设计/数据库修改1.sql
@@ -1,8 +1,10 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2017/4/30 9:17:27                            */
+/* Created on:     2017/5/2 21:37:48                            */
 /*==============================================================*/
 
+
+drop table if exists "Sensitive keyword";
 
 drop table if exists class;
 
@@ -11,6 +13,15 @@ drop table if exists danmu;
 drop table if exists teacher;
 
 drop table if exists user;
+
+/*==============================================================*/
+/* Table: "Sensitive keyword"                                   */
+/*==============================================================*/
+create table "Sensitive keyword"
+(
+   keyword              varchar(32) not null,
+   primary key (keyword)
+);
 
 /*==============================================================*/
 /* Table: class                                                 */
@@ -30,8 +41,10 @@ create table danmu
 (
    dmcontent            varchar(200),
    dmid                 varchar(20) not null,
+   userid               varchar(20),
    classid              varchar(80),
    dmstyle              varchar(20),
+   dmsenddate           varchar(32),
    primary key (dmid)
 );
 
@@ -43,6 +56,7 @@ create table teacher
    teacherid            varchar(32) not null,
    teacherpwd           varchar(200),
    teachername          varchar(32),
+   teachertype          varchar(20),
    primary key (teacherid)
 );
 
@@ -52,8 +66,9 @@ create table teacher
 create table user
 (
    userid               varchar(20) not null,
-   classid              varchar(80),
    userpwd              varchar(20),
+   username             varchar(32),
+   usertype             varchar(20),
    primary key (userid)
 );
 
@@ -63,6 +78,6 @@ alter table class add constraint FK_exists foreign key (teacherid)
 alter table danmu add constraint FK_corresponding foreign key (classid)
       references class (classid) on delete restrict on update restrict;
 
-alter table user add constraint "FK_belong to" foreign key (classid)
-      references class (classid) on delete restrict on update restrict;
+alter table danmu add constraint FK_send foreign key (userid)
+      references user (userid) on delete restrict on update restrict;
 
